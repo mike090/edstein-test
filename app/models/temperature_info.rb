e05ac6c11c6c6cf6b54530d724617eeee624eb6c
mike09 <mike09@mail.ru>
2023-01-25 12:15:55 +0300

    Add model validations

--- app/models/temperature_info.rb
+++ app/models/temperature_info.rb
@@ -1,9 +1,13 @@
 # frozen_string_literal: true
 
 class TemperatureInfo < ApplicationRecord
+  validates :location, presence: true
+  validates :time, presence: true
+  validates :value, presence: true
+
   validates :time, uniqueness: { scope: :location }
 
   scope :by_location, ->(location) { where location: location }
-  scope :ago, ->(duration) { where 'time > ?', Time.now.to_i - duration }
+  scope :ago, ->(duration) { where 'time > ?', Time.now.ago(duration).to_i }
   scope :between, ->(after, before) { where 'time BETWEEN ? AND ?', after, before }
 end
--- db/migrate/20230124055529_create_temperature_infos.rb
+++ db/migrate/20230124055529_create_temperature_infos.rb
@@ -1,9 +1,9 @@
 class CreateTemperatureInfos < ActiveRecord::Migration[6.1]
   def change
     create_table :temperature_infos do |t|
-      t.integer :location
-      t.integer :time
-      t.float :value
+      t.integer :location, null: false
+      t.integer :time, null: false
+      t.float :value, null: false
       t.string :unit
 
       t.timestamps
